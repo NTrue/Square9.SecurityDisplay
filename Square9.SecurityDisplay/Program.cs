4d93cc4449e0de80710b99ab89c5278c8feae263
ntrue <ntrue@square-9.com>
2017-03-07 11:20:51 -0500

    Now runs as a tray app.

--- Square9.SecurityDisplay/Program.cs
+++ Square9.SecurityDisplay/Program.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Net.Http;
 using Microsoft.Owin.Hosting;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace Square9.SecurityDisplay
 {
-    public class Program
+    public class Program : Form
     {
         static void Main()
         {
@@ -16,6 +18,7 @@ namespace Square9.SecurityDisplay
             // Start OWIN host 
             WebApp.Start<Startup>(url: baseAddress);
             System.Diagnostics.Process.Start(baseAddress);
+            
             // Create HttpCient and make a request to api/values 
             HttpClient client = new HttpClient();
 
@@ -23,10 +26,55 @@ namespace Square9.SecurityDisplay
 
                 Console.WriteLine(response);
                 Console.WriteLine(response.Content.ReadAsStringAsync().Result);
-            
 
+            Application.Run(new Program());
             Console.ReadLine();
 
+        }
+
+        private NotifyIcon trayIcon;
+        private ContextMenu trayMenu;
+
+        public Program()
+        {
+            // Create a simple tray menu with only one item.
+            trayMenu = new ContextMenu();
+            trayMenu.MenuItems.Add("Exit", OnExit);
+
+            // Create a tray icon. In this example we use a
+            // standard system icon for simplicity, but you
+            // can of course use your own custom icon too.
+            trayIcon = new NotifyIcon();
+            trayIcon.Text = "MyTrayApp";
+            trayIcon.Icon = new Icon(SystemIcons.Application, 40, 40);
+
+            // Add menu to tray icon and show it.
+            trayIcon.ContextMenu = trayMenu;
+            trayIcon.Visible = true;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Visible = false; // Hide form window.
+            ShowInTaskbar = false; // Remove from taskbar.
+
+            base.OnLoad(e);
+        }
+
+        private void OnExit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                // Release the icon resource.
+                trayIcon.Dispose();
+            }
+
+            base.Dispose(isDisposing);
         }
     }
 }
