ead9ff135678424644a027a3e4ba0ad361f13159
ntrue <ntrue@square-9.com>
2017-03-13 09:47:06 -0400

    Added export button to display page and added Export Controller and Logic.

---
+++ Square9.SecurityDisplay/Controllers/ExportController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Square9.SecurityDisplay.Controllers
+{
+    [RoutePrefix("export")]
+    public class ExportController : ApiController
+    {
+        [HttpPost]
+        [ActionName("exportDocuments")]
+        public HttpResponseMessage ExportSecurityData([FromBody]List<Models.Permissions> permissions, String FilePath)
+        {
+            try
+            {
+                Logic.ExportLogic export = new Logic.ExportLogic();
+                export.exportData(permissions, FilePath);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, "Data Exported Successfully.");
+        }
+    }
+}
---
+++ Square9.SecurityDisplay/Logic/ExportLogic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStack.Text;
+using System.IO;
+
+namespace Square9.SecurityDisplay.Logic
+{
+    public class ExportLogic
+    {
+        public void exportData(List<Models.Permissions> Permissions, String FilePath)
+        {
+            try
+            {
+                string csv = CsvSerializer.SerializeToCsv(Permissions);
+                File.WriteAllText(FilePath, csv);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            
+        }
+    }
+}
